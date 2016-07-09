f1a85b30f4cf8f674ee55de9e131f86856635620
JulianThijssen <Nimthora@gmail.com>
2016-07-09 19:02:55 +0200

    Settings default OpenGL context

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include <QSurfaceFormat>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSurfaceFormat format;
+    format.setDepthBufferSize(24);
+    format.setStencilBufferSize(8);
+    format.setVersion(3, 2);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
+
     MainWindow w;
     w.show();
 
