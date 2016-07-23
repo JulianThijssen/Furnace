75708ff321e790fd7a779da0924720591b8ea9a2
JulianThijssen <Nimthora@gmail.com>
2016-07-23 20:47:00 +0200

    [issue #5] The window can now be moved

--- src/widgets/titlebar.cpp
+++ src/widgets/titlebar.cpp
@@ -2,15 +2,37 @@
 
 #include <QStyleOption>
 #include <QPainter>
+#include <QDebug>
 
-TitleBar::TitleBar(QWidget *parent) : QWidget(parent)
+TitleBar::TitleBar(QWidget *parent) :
+    QWidget(parent),
+    moving(false)
 {
 
 }
 
 void TitleBar::paintEvent(QPaintEvent *event) {
+    Q_UNUSED(event);
     QStyleOption opt;
     opt.init(this);
     QPainter p(this);
     style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+}
+
+void TitleBar::mousePressEvent(QMouseEvent *event) {
+    pos = event->pos();
+    moving = true;
+}
+
+void TitleBar::mouseReleaseEvent(QMouseEvent *event) {
+    Q_UNUSED(event);
+    moving = false;
+}
+
+void TitleBar::mouseMoveEvent(QMouseEvent *event) {
+    if (moving) {
+        QPoint diff = event->pos() - pos;
+        QPoint newPos = parentWidget()->parentWidget()->pos() + diff;
+        parentWidget()->parentWidget()->move(newPos);
+    }
 }
--- src/widgets/titlebar.h
+++ src/widgets/titlebar.h
@@ -2,6 +2,8 @@
 #define TITLEBAR_H
 
 #include <QWidget>
+#include <QMouseEvent>
+#include <QPoint>
 
 class TitleBar : public QWidget
 {
@@ -10,10 +12,16 @@ public:
     explicit TitleBar(QWidget *parent = 0);
 protected:
     void paintEvent(QPaintEvent *event);
+private:
+    bool moving;
+    QPoint pos;
 
 signals:
 
 public slots:
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
 };
 
 #endif // TITLEBAR_H
